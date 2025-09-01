1d13cb9138dcf67219a84d24ee27fd61656f3c8c
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-09-01 20:01:23 +0530

    new

--- js/gsap.js
+++ js/gsap.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 var tl = new gsap.timeline();
 
 tl.from(".site_header", 1, { y: -100, ease: "power1.inOut" });
@@ -11,10 +12,25 @@ tl.from(
     rotate: 70,
     ease: "power1.inOut",
     onComplete: () => {
-      tl.to([".copy1 h3, .copy1 p, .copy2 h3, .copy2 p, .copy2 .icon"], {
+      tl.to([".copy1 h3, .copy1 p, .copy2 h3, .copy2 p, .copy2 .icon"], 0.5, {
         autoAlpha: 1,
+        ease: "power1.inOut",
       });
     },
   },
   1
 );
+const text = document.querySelector(".text");
+const words = text.innerText.split(" ");
+text.innerHTML = words.map((w) => `<span>${w}</span>`).join(" ");
+gsap.to(".text span", {
+  scrollTrigger: {
+    trigger: ".text",
+    start: "top 30%",
+    end: "bottom 150%",
+    scrub: true,
+  },
+  color: "#ff4d4d",
+  stagger: 0.2, // delay between words
+  ease: "none",
+});
